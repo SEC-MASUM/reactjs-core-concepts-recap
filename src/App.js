a64fee6bfc77af4bad319cd25c3be7e790cde60e
SEC-MASUM <masum.jess42@gmail.com>
2022-03-21 23:58:05 +0600

    Practice reactjs core concepts with few examples(recap)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadPosts></LoadPosts>
+      <District name="NuaKhali" spacial="bivag"></District>
+      <District name="Bramonbaria" spacial="joddha Akbor"></District>
+      <District name="Sumilla" spacial="moyna and moti"></District>
+    </div>
+  );
+}
+
+function LoadPosts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://jsonplaceholder.typicode.com/users/1/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []);
+  return (
+    <div>
+      <h1>Posts: {posts.length}</h1>
+      {posts.map((post) => (
+        <Post title={post.title} body={post.body}></Post>
+      ))}
+    </div>
+  );
+}
+function Post(props) {
+  return (
+    <div
+      style={{
+        backgroundColor: "lightcyan",
+        margin: "20px",
+        border: "4px solid salmon",
+      }}
+    >
+      <h2>Title: {props.title}</h2>
+      <hp>Body: {props.body}</hp>
+    </div>
+  );
+}
+
+const districtStyle = {
+  backgroundColor: "orange",
+  margin: "20px",
+  padding: "10px",
+  borderRadius: "5px",
+};
+
+function District(props) {
+  const [power, setPower] = useState(1);
+  const boostPower = () => {
+    const newPower = power * 2;
+    setPower(newPower);
+  };
+
+  return (
+    <div style={districtStyle}>
+      <h3>Name: {props.name}</h3>
+      <p>Specialty: {props.spacial}</p>
+      <h2>Power: {power}</h2>
+      <button onClick={boostPower}>Boost The Power</button>
     </div>
   );
 }
